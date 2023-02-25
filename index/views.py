248842ed6c2ce4c80301581ad9b8969027349fc4
eulerBM <eullerborgesdamotta155@gmail.com>
2023-02-25 03:54:19 -0300

    carrosel das noticias sobre covid

--- index/views.py
+++ index/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
-    return render (request, 'index.html')
+    api_key = '6284481ecd2c40ad955ad3b2873a2a73'
+    url = 'https://newsapi.org/v2/top-headlines?country=br&category=health&apiKey=' + api_key
+    response = requests.get(url)
+    articles = response.json()['articles']
+     
+    context = {
+        'articles': articles
+    }
+
+    return render (request, 'index.html', context)
 
 
