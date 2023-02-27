7c84ff307776fe1ed2951b74619a3811a36e53cc
eulerBM <eullerborgesdamotta155@gmail.com>
2023-02-27 05:06:06 -0300

    usuario receber noticias via e-mail

--- news/views.py
+++ news/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+import requests
 
 def news(request):
-    return render (request, 'news.html')
+
+    if request.method == 'GET':
+        api_key = '6284481ecd2c40ad955ad3b2873a2a73'
+        url = 'https://newsapi.org/v2/top-headlines?country=br&category=health&apiKey=' + api_key
+        response = requests.get(url)
+        articles = response.json()['articles']
+
+        context = {
+            'articles': articles,
+            
+        }
+
+    return render (request, 'news.html', context)
